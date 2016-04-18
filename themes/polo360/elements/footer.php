5615d3476d401275522be238e8d4adf6a05f7eba
nicolas <nicolas.pavageau@gmail.com>
2016-04-18 20:29:28 +0200

    ajout de footer.php dans elements

---
+++ themes/polo360/elements/footer.php
@@ -0,0 +1,23 @@
+
+<footer>
+  <p>
+    Site Powered by Wordpress. Design and Developped by VIVROCKS.
+  </p>
+  <p>
+  Copyright 2010. Studio VIVROCKS. All rights Reserved.
+  </p>
+  <div id="links">
+    <a>home</a>
+    <a>about</a>
+    <a>products</a>
+    <a>service</a>
+    <a>contact</a>
+    <a>privacy policy</a>
+    <a>terms of use</a>
+  </div>
+
+</footer>
+  </div>
+  <?php Loader::element('footer_required'); ?>
+</body>
+</html>
