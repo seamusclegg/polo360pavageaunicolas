7ca9e5d0bd15f7861b975ff632ebd6f8188b2684
nicolas <nicolas.pavageau@gmail.com>
2016-04-19 20:51:52 +0200

    modifications sur footer.php

--- themes/polo360/elements/footer.php
+++ themes/polo360/elements/footer.php
@@ -1,19 +1,19 @@
 
-<footer>
-  <p>
+<footer class="row">
+  <p class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
     Site Powered by Wordpress. Design and Developped by VIVROCKS.
   </p>
-  <p>
+  <p class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
   Copyright 2010. Studio VIVROCKS. All rights Reserved.
   </p>
-  <div id="links">
-    <a>home</a>
-    <a>about</a>
-    <a>products</a>
-    <a>service</a>
-    <a>contact</a>
-    <a>privacy policy</a>
-    <a>terms of use</a>
+  <div id="links" class="row">
+    <a href="#" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-lg-offset-1" id="ahome">home</a>
+    <a href="#" class="col-xs-1 col-sm-1 col-md-1 col-lg-1">about</a>
+    <a href="#" class="col-xs-1 col-sm-1 col-md-1 col-lg-1">products</a>
+    <a href="#" class="col-xs-1 col-sm-1 col-md-1 col-lg-1">service</a>
+    <a href="#" class="col-xs-1 col-sm-1 col-md-1 col-lg-1">contact</a>
+    <a href="#" class="col-xs-2 col-sm-2 col-md-2 col-lg-2">privacy policy</a>
+    <a href="#" class="col-xs-2 col-sm-2 col-md-2 col-lg-2">terms of use</a>
   </div>
 
 </footer>
